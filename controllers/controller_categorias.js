7140eb4fb054596512d7f688788e2a4b1d4b8a72
Weildor <240065@utxicotepec.edu.mx>
2026-04-12 19:10:51 -0600

    CRUD de Usuarios y Categorias funcional junto con MySQL y corregido (en controller_categorias.js GET)

--- controllers/controller_categorias.js
+++ controllers/controller_categorias.js
@@ -1,3 +1,57 @@
+const { tbc_categorias } = require('../models'); 
+
+module.exports = {
+    create(req, res){
+        return tbc_categorias
+            .create({
+                nombre: req.body.nombre,
+                descripcion: req.body.descripcion // Añadido descripción
+            })
+            .then(categoria => res.status(201).send(categoria)) 
+            .catch(error => res.status(400).send(error));
+    },
+    
+    list(_, res){
+        return tbc_categorias.findAll({})
+            .then(categorias => res.status(200).send(categorias))
+            .catch(error => res.status(400).send(error));
+    },
+    
+    find(req, res){
+        // Cambiado a findByPk y usando req.params.id
+        return tbc_categorias.findByPk(req.params.id)
+            .then(categoria => {
+                if (!categoria) {
+                    return res.status(404).send({ message: "Categoría no encontrada" });
+                }
+                return res.status(200).send(categoria);
+            })
+            .catch(error => res.status(400).send(error));
+    },
+    
+    delete(req, res){
+        return tbc_categorias.destroy({
+            where: { id: req.params.id }
+        })
+        .then(() => res.status(200).send({message: "Categoría eliminada correctamente"}))
+        .catch(error => res.status(400).send(error));
+    },
+    
+    update(req, res){
+        return tbc_categorias.update(
+            {
+                nombre: req.body.nombre,
+                descripcion: req.body.descripcion // Añadido descripción
+            },
+            {
+                where: { id: req.params.id }
+            }
+        )
+        .then(() => res.status(200).send({message: "Categoría actualizada correctamente"}))
+        .catch(error => res.status(400).send(error));
+    }
+};
+/*
 // Importamos directamente el modelo desde la carpeta models.
 // NOTA: Asegúrate de que 'tbc_categorias' sea el nombre exacto con el que 
 // se exporta tu modelo. Si falla, podría llamarse 'tbc_categoria' o 'Categoria'.
@@ -55,6 +109,9 @@ module.exports = {
         .catch(error => res.status(400).send(error));
     }
 };
+*/
+
+/////////////////////////////////////////////////////////////////
 /*const Sequelize = require('sequelize');
 const categoria = require('../models').categoria;
 const categoria = db.tbc_categoria;
--- routes/route_categorias.js
+++ routes/route_categorias.js
@@ -3,8 +3,8 @@ const categoriaController = require('../controllers/controller_categorias');
 module.exports = (app) => {
     
     app.get('/api/categorias', categoriaController.list);
-    app.get('/api/categoria/:nombre', categoriaController.find);
-    app.post('/api/categoria/', categoriaController.create);
+    app.get('/api/categoria/:id', categoriaController.find);
+    app.post('/api/categorias', categoriaController.create);
     app.put('/api/categoria/:id', categoriaController.update);
     app.delete('/api/categoria/:id', categoriaController.delete);
 }
